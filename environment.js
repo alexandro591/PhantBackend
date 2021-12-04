d13a2caa7801aa7aa8d7f79f271228e741a4b80f
alexandro591 <alexandrotapiaflores@gmail.com>
2021-12-04 01:39:17 -0500

    change integration names to use actual names remove backendUrl environment variable to use req

--- environment.js
+++ environment.js
@@ -2,5 +2,4 @@ require('dotenv').config();
 
 module.exports = {
   chatAppUrl: process.env.CHAT_APP_URL,
-  backendUrl: process.env.BACKEND_URL,
 };
---
+++ pages/api/v1/integration/site/[site]/version/[version]/index.js
@@ -0,0 +1,23 @@
+const environment = require('../../../../../../../../environment');
+
+export default function handler(req, res) {
+  const htmlTemplate = require(`../../../../../../../../public/integration/${req.query.version}/integration.handlebars.html`);
+  const jsTemplate = require(`../../../../../../../../public/integration/${req.query.version}/integration.handlebars.js`);
+
+  const backendUrl = req.headers.host.includes(':')
+    ? `http://${req.headers.host}`
+    : `https://${req.headers.host}`;
+
+  const cssUrl = `${backendUrl}/integration/${req.query.version}/integration.handlebars.css`;
+
+  const phantChatHTML = htmlTemplate({ cssUrl });
+
+  const javascript = jsTemplate({
+    site: req.query.site,
+    chatAppUrl: environment.chatAppUrl,
+    phantChatHTML: phantChatHTML,
+    backgroundBubbleImage: `${backendUrl}/images/chat.svg`,
+  });
+
+  res.send(javascript);
+}
